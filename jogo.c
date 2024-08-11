823740a2493767dee5b80993ccc3a4bd6c34b18b
yhamerith <gabriel.santos.marcar1@gmail.com>
2024-08-11 08:32:05 -0300

    Refatoração

--- jogo.c
+++ jogo.c
@@ -7,52 +7,63 @@ void intro() {
     printf("*********************\n\n");
 }
 
-void chute(char c [26], int t) {
+void chute(char chutes[26], int* tentativas) {
     char chute;
     scanf(" %c", &chute);
 
-    c[t] = chute;
+    chutes[(*tentativas)] = chute;
+    (*tentativas)++;
+}
+
+int chutou(char letra, char chutes[26], int tentativas){
+    int achou = 0;
+
+    for(int j=0; j<tentativas; j++) {
+        if(chutes[j] == letra) {
+            achou = 1;
+            break;
+        }
+    }
+
+    return achou;
+}
+
+void desenhaforca(char palavrasecreta[20], char chutes[26], int tentativas) {
+    for(int i=0; i<strlen(palavrasecreta); i++) {
+
+        int retornoachou = chutou(palavrasecreta[i], chutes, tentativas);
+
+        if(retornoachou) {
+            printf("%c ", palavrasecreta[i]);
+        } else {
+            printf("_ ");
+        }
+    }
+    printf("\n");
+}
+
+void palavraselecionada(char palavrasecreta[20]) {
+    sprintf(palavrasecreta, "MELANCIA");
 }
 
 int main () {
-    
     //Letras & Palavras
     char palavrasecreta[20];
-
-    sprintf(palavrasecreta, "MELANCIA");
-
+    char chutes [26];
+    int tentativas = 0;
     int acertou = 0;
     int enforcou = 0;
 
-    char chutes [26];
-    int tentativas = 0;
 
+
+    palavraselecionada(palavrasecreta);
     intro();
     
     do {
         //O jogo
 
-        for(int i=0; i<strlen(palavrasecreta); i++) {
-
-            int achou = 0;
-
-            for(int j=0; j<tentativas; j++) {
-                if(chutes[j] == palavrasecreta[i]) {
-                    achou = 1;
-                    break;
-                }
-            }
-            
-            if(achou) {
-                printf("%c ", palavrasecreta[i]);
-            } else {
-                printf("_ ");
-            }
-        }
-        printf("\n");
-
-        chute(chutes, tentativas);
-        tentativas++;
+        desenhaforca(palavrasecreta, chutes, tentativas);
+        chute(chutes, &tentativas);
         
     } while(!acertou && !enforcou );
     
